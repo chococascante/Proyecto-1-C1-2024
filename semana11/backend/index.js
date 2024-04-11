dc52dc0499eaf9e752659fd0ac463e06cdb287f7
Luis Cascante <luisalonzocascante@gmail.com>
2024-04-10 19:49:10 -0600

    mitad clase

--- semana11/backend/index.js
+++ semana11/backend/index.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(cors());
 
+// Permitir que Express entienda el cuerpo de las solicitudes
+app.use(express.json());
+
 mongoose.connect(
   "mongodb+srv://lcascante:cXe8bvl9pDpvWyeo@cluster0.isjhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
@@ -21,45 +24,126 @@ const port = 3000;
 // PUT: actualizar información
 // DELETE: eliminar información
 
-const usuarios = [
-  {
-    id: 1,
-    nombre: "Juan",
-    apellido: "Perez",
-    edad: 30,
-  },
-  {
-    id: 2,
-    nombre: "Maria",
-    apellido: "Gomez",
-    edad: 25,
-  },
-  {
-    id: 3,
-    nombre: "Carlos",
-    apellido: "Lopez",
-    edad: 40,
-  },
-];
-
 app.get("/usuarios", async function (req, res) {
+  console.log("Atentiendo solicitud GET a ruta /usuarios");
   try {
     const users = await modeloUsuario.find({});
+    console.log("Usuarios obtenidos", users);
     res.status(200).send(users);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).send("Hubo un error al obtener los usuarios");
   }
 });
 
-app.get("/usuarios/:id", function (req, res) {
+app.get("/usuarios/:id", async function (req, res) {
   const id = req.params.id;
+  console.log("Atentiendo solicitud GET a ruta /usuarios/:id, con id:", id);
 
-  const usuario = usuarios.find(function (usuario) {
-    return usuario.id == id;
-  });
+  try {
+    const usuario = await modeloUsuario.findById(id);
+    // const usuarioPorCedula = modeloUsuario.find({
+    //   cedula: id,
+    //   correo: "lcascante@ucenfotec.ac.cr"
+    // });
+    res.send(usuario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al obtener el usuario");
+  }
+});
 
-  res.send(usuario);
+app.delete("/usuarios/:id", async function (req, res) {
+  const id = req.params.id;
+  console.log("Atentiendo solicitud DELETE a ruta /usuarios/:id, con id:", id);
+
+  try {
+    const usuario = await modeloUsuario.findByIdAndDelete(id);
+
+    res.status(204).send(usuario.id);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al obtener el usuario");
+  }
+});
+
+app.post("/usuarios", async function (req, res) {
+  console.log("Atentiendo solicitud POST a ruta /usuarios", req.body);
+
+  // Validar que se envió el cuerpo de la solicitud
+  if (
+    !req.body.nombre ||
+    !req.body.apellido ||
+    !req.body.cedula ||
+    !req.body.email ||
+    !req.body.password
+  ) {
+    console.error("No se envió el cuerpo de la solicitud");
+    res.status(400).send("Falta el cuerpo de la solicitud");
+  }
+
+  try {
+    // Crear un nuevo usuario
+    const nuevoUsuario = new modeloUsuario({
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      cedula: req.body.cedula,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Guardar el usuario en la base de datos
+    await nuevoUsuario.save();
+
+    // Responder con el ID del usuario creado
+    res.status(201).send(nuevoUsuario.id);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al crear el usuario");
+  }
+});
+
+app.patch("/usuarios", async function (req, res) {
+  console.log("Atentiendo solicitud PATCH a ruta /usuarios", req.body);
+
+  // Validar que se envió el cuerpo de la solicitud
+  if (!req.body) {
+    console.error("No se envió el cuerpo de la solicitud");
+    res.status(400).send("Falta el cuerpo de la solicitud");
+  }
+
+  try {
+    // Paso 1: Buscar el usuario por ID en la base (datos viejos)
+    const usuario = await modeloUsuario.findById(req.body.id);
+    // Paso 2: Actualizar el usuario con los datos nuevos
+    const usuarioActualizado = await modeloUsuario.findByIdAndUpdate(
+      req.body.id,
+      {
+        nombre: req.body.nombre ?? usuario.nombre,
+        apellido: req.body.apellido ?? usuario.apellido,
+        cedula: req.body.cedula ?? usuario.cedula,
+        email: req.body.email ?? usuario.email,
+      }
+    );
+
+    // const usuarioActualizado2 = await modeloUsuario.findOneAndUpdate(
+    //   {
+    //     cedula: req.body.cedula,
+    //   },
+    //   {
+    //     nombre: req.body.nombre ?? usuario.nombre,
+    //     apellido: req.body.apellido ?? usuario.apellido,
+    //     cedula: req.body.cedula ?? usuario.cedula,
+    //     email: req.body.email ?? usuario.email,
+    //   }
+    // );
+
+    // Responder con el ID del usuario actualizado
+    res.status(200).send(usuarioActualizado.id);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al actualizar el usuario");
+  }
 });
 
 app.listen(port, function () {
--- semana11/backend/models/users.js
+++ semana11/backend/models/users.js
@@ -1,25 +1,34 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    cedula: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    nombre: {
+      type: String,
+      required: true,
+    },
+    apellido: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  cedula: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  nombre: {
-    type: String,
-    required: true,
-  },
-  apellido: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
